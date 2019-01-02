f4d79a083e78fdc9b6c9e24bfc6eafe1c70202a3
godspeedkil <edgarjdiazm@gmail.com>
2019-01-01 20:37:01 -0800

    first commit fix

---
+++ background/helpers/IceServersHandler.js
@@ -0,0 +1,16 @@
+var IceServersHandler = (function() {
+    function getIceServers(connection) {
+        return [{
+            'urls': [
+                'stun:stun.l.google.com:19302',
+                'stun:stun1.l.google.com:19302',
+                'stun:stun2.l.google.com:19302',
+                'stun:stun.l.google.com:19302?transport=udp',
+            ]
+        }];
+    }
+
+    return {
+        getIceServers: getIceServers
+    };
+})();
